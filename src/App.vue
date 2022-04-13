ad653830c33fb9b0a4c28a77d3efce101cefe3b5
Arnaud-Ferreira <ferreira.arnaud@hotmail.fr>
2022-04-13 19:13:51 +0200

    setting up the structure || components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <contenu />
+    <header-top />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Content from './components/Content.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'contenu': Content,
+    'header-top': Header
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div class="container mt-5">
+
+     
+    </div>
+
+
+</template>
+
+
+
+<script>
+
+    export default {
+        name: 'ContenuComp',
+        data(){
+            return {
+            }
+        },
+        components: {
+        }
+        
+    }
+
+</script>
+
+<style scoped>
+
+    h1 {
+        margin-top: 100px !important;
+    }
+    ul {
+        margin-top: 1rem;
+        list-style-type: none;
+        padding: 0;
+    }
+
+</style>
