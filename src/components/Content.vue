1386037bc2c215908a9fa4b78e87af58db812ae7
Arnaud-Ferreira <ferreira.arnaud@hotmail.fr>
2022-04-13 19:36:31 +0200

    default dynamic display of tasks

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -2,23 +2,46 @@
 
     <div class="container mt-5">
 
-     
+     <h1>Enter tasks to be done</h1>
+
+    <form>
+
+        <div class="form-group">
+            <label for="action">Action</label>
+            <input type="text" id="action" class="form-control mt-2">
+        </div>
+
+        <button class="btn btn-primary mt-3">Create a task</button>
+
+    </form>
+
+    <ul>
+        <li v-bind:key="index" v-for="(task, index) in taskArray">
+            <task :task="task"/>
+        </li>
+    </ul>
+
     </div>
-
-
 </template>
 
 
 
 <script>
 
+    import Task from './Task.vue';
+
     export default {
         name: 'ContenuComp',
         data(){
             return {
+                formData: {
+                    task: ''
+                },
+                taskArray: ['JavaScript', 'Learn Vue', 'Learn Python', 'Master React']
             }
         },
         components: {
+            'task': Task
         }
         
     }
---
+++ src/components/Task.vue
@@ -0,0 +1,40 @@
+<template>
+    
+    <div class="card my-2 item-todo">
+       <p>{{ task }}</p> 
+       <div class="btn-supr btn btn-danger">X</div>
+    </div>
+
+
+</template>
+
+
+<script>
+
+    export default {
+        name: 'TaskComp',
+        props: ['task', 'delete']
+    }
+
+</script>
+
+
+<style scoped>
+
+    .item-todo{
+        position: relative;
+        padding: 20px 60px 20px 30px;
+        font-size: 20px;
+        height: 88px;
+    }
+
+    .btn-supr{
+        position: absolute;
+        top: 10px;
+        right: 10px;
+    }
+
+    .item-todo p {
+        margin-bottom: 0!important;
+    }
+</style>
